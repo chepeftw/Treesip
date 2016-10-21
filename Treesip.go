3db88b2817c86227f5ecaf7685d5963b3306b3d6
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2016-10-21 15:45:36 +0200

    Minmizing time after the election of neo.

--- Treesip.go
+++ Treesip.go
@@ -376,7 +376,7 @@ func selectLeaderOfTheManet() {
         }
 
         log.Info("The leader has been chosen!!! All hail the new KING!!! " + neo)
-        time.Sleep(time.Second * 5)
+        time.Sleep(time.Second * 3)
 
         js, err := json.Marshal(payload)
         utils.CheckError(err, log)
